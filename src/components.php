62eb05a0f49339a67b062a87898ca87e123ee4db
Kamil Balwierz <kamil@snow.dog>
2016-08-01 11:05:01 +0200

    Introduced some mess for happy merging

--- src/components.php
+++ src/components.php
@@ -1,39 +1,36 @@
 <?php
 
-use Snowdog\DevTest\Command\MigrateCommand;
-use Snowdog\DevTest\Command\WarmCommand;
-use Snowdog\DevTest\Component\CommandRepository;
-use Snowdog\DevTest\Component\Menu;
-use Snowdog\DevTest\Component\Migrations;
-use Snowdog\DevTest\Component\RouteRepository;
-use Snowdog\DevTest\Controller\CreatePageAction;
-use Snowdog\DevTest\Controller\CreateWebsiteAction;
-use Snowdog\DevTest\Controller\IndexAction;
-use Snowdog\DevTest\Controller\LoginAction;
 use Snowdog\DevTest\Controller\LoginFormAction;
+use Snowdog\DevTest\Component\Migrations;
 use Snowdog\DevTest\Controller\LogoutAction;
-use Snowdog\DevTest\Controller\RegisterAction;
-use Snowdog\DevTest\Controller\RegisterFormAction;
-use Snowdog\DevTest\Controller\WebsiteAction;
-use Snowdog\DevTest\Menu\LoginMenu;
-use Snowdog\DevTest\Menu\RegisterMenu;
 use Snowdog\DevTest\Menu\WebsitesMenu;
+use Snowdog\DevTest\Controller\CreatePageAction;
+use Snowdog\DevTest\Component\RouteRepository;
+use Snowdog\DevTest\Command\WarmCommand;
+use Snowdog\DevTest\Controller\CreateWebsiteAction;
+use Snowdog\DevTest\Command\MigrateCommand;
+use Snowdog\DevTest\Component\Menu;
+use Snowdog\DevTest\Controller\RegisterFormAction;
+use Snowdog\DevTest\Controller\IndexAction;
+use Snowdog\DevTest\Menu\LoginMenu;
+use Snowdog\DevTest\Component\CommandRepository;
+use Snowdog\DevTest\Controller\WebsiteAction;
+use Snowdog\DevTest\Menu\RegisterMenu;
+use Snowdog\DevTest\Controller\LoginAction;
+use Snowdog\DevTest\Controller\RegisterAction;
 
-RouteRepository::registerRoute('GET', '/', IndexAction::class, 'execute');
+Menu::register(RegisterMenu::class, 250);
+CommandRepository::registerCommand('migrate_db', MigrateCommand::class);
+Menu::register(WebsitesMenu::class, 10);
+RouteRepository::registerRoute('POST', '/website', CreateWebsiteAction::class, 'execute');
 RouteRepository::registerRoute('GET', '/login', LoginFormAction::class, 'execute');
-RouteRepository::registerRoute('POST', '/login', LoginAction::class, 'execute');
+RouteRepository::registerRoute('POST', '/register', RegisterAction::class, 'execute');
+RouteRepository::registerRoute('GET', '/', IndexAction::class, 'execute');
+Migrations::registerComponentMigration('Snowdog\\DevTest', 2);
+RouteRepository::registerRoute('POST', '/page', CreatePageAction::class, 'execute');
+CommandRepository::registerCommand('warm [id]', WarmCommand::class);
 RouteRepository::registerRoute('GET', '/logout', LogoutAction::class, 'execute');
 RouteRepository::registerRoute('GET', '/register', RegisterFormAction::class, 'execute');
-RouteRepository::registerRoute('POST', '/register', RegisterAction::class, 'execute');
-RouteRepository::registerRoute('GET', '/website/{id:\d+}', WebsiteAction::class, 'execute');
-RouteRepository::registerRoute('POST', '/website', CreateWebsiteAction::class, 'execute');
-RouteRepository::registerRoute('POST', '/page', CreatePageAction::class, 'execute');
-
-CommandRepository::registerCommand('migrate_db', MigrateCommand::class);
-CommandRepository::registerCommand('warm [id]', WarmCommand::class);
-
 Menu::register(LoginMenu::class, 200);
-Menu::register(RegisterMenu::class, 250);
-Menu::register(WebsitesMenu::class, 10);
-
-Migrations::registerComponentMigration('Snowdog\\DevTest', 2);
+RouteRepository::registerRoute('POST', '/login', LoginAction::class, 'execute');
+RouteRepository::registerRoute('GET', '/website/{id:\d+}', WebsiteAction::class, 'execute');
